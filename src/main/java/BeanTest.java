f8cd87c49ed4e372798afaa21cfecb35670f3abc
Scott Roberts <scott.w.roberts70@gmail.com>
2022-07-18 14:26:03 -0500

    finished exercises

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -1,6 +1,7 @@
-package beans;
-
 import beans.Album;
+import beans.Artist;
+import beans.Author;
+import beans.Quotes;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,17 +9,17 @@ import java.util.Date;
 public class BeanTest {
 
     public static void main(String[] args) {
-        Author author = new Author("Scott", "Roberts");
-        Author author2 = new Author("Mike", "Friend");
-        Author author3 = new Author("Dillon", "Miller");
+        Author author = new Author(1,"Scott", "Roberts");
+        Author author2 = new Author(2,"Mike", "Friend");
+        Author author3 = new Author(3, "Dillon", "Miller");
 
         Artist artist = new Artist("Mike", false, "English");
         Artist artist2 = new Artist("Jake", true, "English");
         Artist artist3 = new Artist("MacDillon", false, "English");
 
-        Album album = new Album(artist,"Mike", new Date(-2022), 3.5, "R&B");
-        Album album2 = new Album(artist2,"Jake", new Date(-2022), 1.5, "Rap");
-        Album album3 = new Album(artist3,"MacDillon", new Date(-2022), 4.5, "Rock");
+        Album album = new Album(artist,"Mike", new Date("1/1/2021"), 3.5, "R&B");
+        Album album2 = new Album(artist2,"Jake", new Date("1/1/2021"), 1.5, "Rap");
+        Album album3 = new Album(artist3,"MacDillon", new Date("1/1/2021"), 4.5, "Rock");
 
         Quotes quotes = new Quotes("blah blah blah",  author);
         Quotes quotes1 = new Quotes("haha haha haha",  author2);
