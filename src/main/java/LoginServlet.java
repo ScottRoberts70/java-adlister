40cc61ddc73c5521eec2878170ef051f644d0df5
Scott Roberts <scott.w.roberts70@gmail.com>
2022-07-19 14:00:40 -0500

    Updated error with sesssion in my if statement

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -11,20 +11,20 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         if (session.getAttribute( "user" ) == null) {
-            req.getRequestDispatcher("WEB-INF/login.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
         } else {
             resp.sendRedirect("/profile");
         }
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        HttpSession session = req.getSession();
         String user = req.getParameter("user");
         String password = req.getParameter("password");
-        session.setAttribute("user", user);
+        HttpSession session = req.getSession();
         boolean validAttempt = user.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            session.setAttribute("user", user);
             resp.sendRedirect("/profile");
         } else {
             resp.sendRedirect("/login");
